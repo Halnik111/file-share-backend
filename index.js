2fbade21cdf26b4e89c7f997a7fbd636ce64f2ed
halnik111 <halonikolas@gmail.com>
2025-01-08 22:12:28 +0100

    .

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ app.get('/', (req,res) => {res.status(200).json('working!@!')});
 
 const port = Number(process.env.PORT || 8080);
 app.listen(port, "0.0.0.0", () => {
-    console.log("Connected!");
+    console.log("Connected! " + port);
     //connect();
 });
