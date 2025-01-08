de18f1a4c35cad7c1c9a7b2e9afd2134db93bac3
halnik111 <halonikolas@gmail.com>
2025-01-08 22:29:38 +0100

    .

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use("/files", filesRoutes)
 app.get('/', (req,res) => {res.status(200).json('working!@!')});
 
 
-const port = Number(process.env.PORT || 3001);
+const port = Number(process.env.PORT || 80);
 app.listen(port, "0.0.0.0", () => {
     console.log("Connected! " + port);
     //connect();
